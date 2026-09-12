335d1206b2ee7de66f80d985066388f2951f3348
ISHAAN JHA <ishaanjha.in@gmail.com>
2026-09-12 22:54:35 +0530

    Fix JSX syntax error in Admin Events page

--- web/app/admin/(protected)/events/page.tsx
+++ web/app/admin/(protected)/events/page.tsx
@@ -356,7 +356,7 @@ export default function AdminEvents() {
                     </DndContext>
                 </div>
             </div>
-            </div>
+
 
             {/* Modal */}
             {isModalOpen && (
